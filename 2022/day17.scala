4bd46f14c55a0dcc4d45751367831fb6f5117a13
Albert Sadowski <asadowski@mailbox.org>
2022-12-24 06:55:26 +0000

    Day XVII, cleanup

--- 2022/day17.scala
+++ 2022/day17.scala
@@ -1,6 +1,8 @@
 import scala.collection.mutable
 import scala.io.Source
 
+val PatternOffset = 30
+
 case class Rock(str: String):
     val lines = str.split("\n")
     val width = lines.map(_.length).max
@@ -57,10 +59,17 @@ class Cave(map: mutable.ArrayBuffer[mutable.ArrayBuffer[Char]], jetPattern: Vect
         for (xr, yr) <- rock.iterIx() do
             map(pos._2 - yr)(pos._1 + xr) = '#'
 
-    def print() =
-        for y <- map.length - 1 to 0 by -1 do
-            println("|" + map(y).mkString + "|")
-        println("+-------+")
+    def hash() =
+        var (res, yMax) = ("", map.length)
+        while
+            yMax -= 1
+            isEmpty(map(yMax))
+        do ()
+
+        for y <- yMax to (yMax - PatternOffset) by -1 if y >= 0 do
+            res += map(y).mkString
+
+        res.hashCode()
 
     def canDraw(rock: Rock, pos: (Int, Int)): Boolean =
         for (xr, yr) <- rock.iterIx() do
@@ -95,7 +104,48 @@ def dropN(jetPattern: Vector[Char], n: Int) =
 
     cave.nonEmptyHeight()
 
+def checkCycle(items: mutable.ArrayBuffer[Int]): (Int, Boolean) =
+    for p <- 0 until items.length / 2 do
+        val off = p + ((items.length - p) / 2)
+        val l = items.slice(p, off)
+        val r = items.slice(off, items.length)
+        if l == r then
+            return (p, true)
+    (-1, false)
+
+def partI(pattern: Vector[Char]) = dropN(pattern, 2022)
+
+def partII(pattern: Vector[Char]): Long =
+    val cave = new Cave(mutable.ArrayBuffer(), pattern)
+    val seen = new mutable.ArrayBuffer[Int]()
+    var (ix, rem, res, resHeight) = (0, 0L, 0L, 0)
+    var countRem = false
+
+    for rock <- Rocks do
+        cave.drop(rock)
+        seen.addOne(cave.hash())
+        ix += 1
+
+        if countRem then
+            rem -= 1
+
+            if rem <= 0 then
+                return res + cave.nonEmptyHeight() - resHeight
+        else
+            val (preambleEndIx, cycleFound) = checkCycle(seen)
+            if cycleFound then
+                val preambleHeight = dropN(pattern, preambleEndIx)
+                val cycleIxSpan = (ix - preambleEndIx) / 2
+                val cycleHeight = (cave.nonEmptyHeight() - preambleHeight) / 2
+                resHeight = cave.nonEmptyHeight()
+                rem = (1000000000000L - ix) % cycleIxSpan
+                res = ((1000000000000L - ix) / cycleIxSpan) * cycleHeight + resHeight
+                countRem = true
+
+    throw new Exception("Cycle not found")
+
 @main def main(input: String) =
     val pattern = jetPatternFromInput(input)
 
-    println(s"Part I: ${dropN(pattern, 2022)}")
+    println(s"Part I: ${partI(pattern)}")
+    println(s"Part II: ${partII(pattern)}")
