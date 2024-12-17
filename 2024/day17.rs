b547a5b3962a34da913358deaf5d9574db4028ec
Albert Sadowski <asadowski@mailbox.org>
2024-12-17 08:01:47 +0000

    wip

--- 2024/day17.rs
+++ 2024/day17.rs
@@ -50,7 +50,62 @@ fn evaluate(reg: (i64, i64, i64), program: &Vec<i64>) -> Vec<i64> {
     out
 }
 
+fn solve(program: &Vec<i64>, p: i64, r: i64) -> bool {
+    if p < 0 {
+        println!("r: {}", r);
+        return true;
+    }
+
+    for d in 0..8 {
+        let mut a = r << 3 | d;
+        let mut b = 0;
+        let mut c = 0;
+        let mut i = 0;
+        let mut w = -1;
+        while i < program.len() {
+            let (instr, lit) = (program[i], program[i + 1]);
+            let com = match lit {
+                4 => a,
+                5 => b,
+                6 => c,
+                x if x <= 3 => x,
+                _ => panic!("invalid combo"),
+            };
+
+            match instr {
+                0 => { a >>= com; },
+                1 => { b ^= lit; },
+                2 => { b = com & 7; },
+                3 => {
+                    if a != 0 {
+                        i = lit as usize;
+                        continue;
+                    }
+                },
+                4 => { b ^= c; },
+                5 => { w = com & 7; break; },
+                6 => { b = a >> com; },
+                7 => { c = a >> com; },
+                _ => panic!("invalid instruction"),
+            };
+
+            i += 2;
+        }
+
+        if w == program[p as usize] && solve(program, p - 1, r << 3 | d) {
+            return true;
+        }
+    }
+    false
+}
+
+fn part_2(program: &Vec<i64>) -> i64 {
+    solve(program, program.len() as i64 - 1, 0);
+    0
+}
+
 fn main() {
     let (reg, program) = parse(&aoc::input_lines());
     println!("Part I: {}", join(&evaluate(reg, &program)));
+    println!("Part II: {}", part_2(&program));
 }
