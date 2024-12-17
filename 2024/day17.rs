6b448768b7e07f5d4f40f6b95cff6ba9d59e8d31
Albert Sadowski <asadowski@mailbox.org>
2024-12-17 08:01:47 +0000

    2024, day 17, part II

--- 2024/day17.rs
+++ 2024/day17.rs
@@ -23,8 +23,7 @@ fn evaluate(reg: (i64, i64, i64), program: &Vec<i64>) -> Vec<i64> {
             4 => a,
             5 => b,
             6 => c,
-            x if x <= 3 => x,
-            _ => panic!("invalid combo"),
+            x => x,
         };
 
         match instr {
@@ -50,7 +49,58 @@ fn evaluate(reg: (i64, i64, i64), program: &Vec<i64>) -> Vec<i64> {
     out
 }
 
+fn solve(program: &Vec<i64>, pos: i32, curr: i64) -> Option<i64> {
+    if pos < 0 {
+        return Some(curr);
+    }
+
+    for off in 0..8 {
+        let (mut a, mut b, mut c) = (curr << 3 | off, 0, 0);
+        let mut ip = 0;
+        let mut out = None;
+        while ip < program.len() {
+            let (instr, lit) = (program[ip], program[ip + 1]);
+            let com = match lit {
+                4 => a,
+                5 => b,
+                6 => c,
+                x => x,
+            };
+
+            match instr {
+                0 => { a >>= com; },
+                1 => { b ^= lit; },
+                2 => { b = com & 7; },
+                3 => {
+                    if a != 0 {
+                        ip = lit as usize;
+                        continue;
+                    }
+                },
+                4 => { b ^= c; },
+                5 => {
+                    out = Some(com & 7);
+                    break;
+                },
+                6 => { b = a >> com; },
+                7 => { c = a >> com; },
+                _ => panic!("invalid instruction"),
+            };
+
+            ip += 2;
+        }
+
+        if out == Some(program[pos as usize]) {
+            if let Some(next) = solve(program, pos - 1, curr << 3 | off) {
+                return Some(next);
+            }
+        }
+    }
+    None
+}
+
 fn main() {
     let (reg, program) = parse(&aoc::input_lines());
     println!("Part I: {}", join(&evaluate(reg, &program)));
+    println!("Part II: {}", solve(&program, program.len() as i32 - 1, 0).unwrap());
 }
