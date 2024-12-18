4769ff3700a46610517aaa7c64e3fb520c1e3164
Albert Sadowski <asadowski@mailbox.org>
2024-12-18 23:12:11 +0000

    2024, day 15, part II

--- 2024/day15.rs
+++ 2024/day15.rs
@@ -56,7 +56,64 @@ fn try_push(board: &mut Vec<Vec<char>>, bx: (usize, usize), dir: (i32, i32)) -> 
         return true;
     }
 
-    if board[ny][nx] == '[' || board[ny][nx] == ']' {}
+    (nx, ny) = bx;
+    if board[ny][nx] == '[' || board[ny][nx] == ']' {
+        let mut to_move = vec![];
+        let mut do_move = vec![];
+
+        if board[ny][nx] == '[' {
+            to_move.push((nx, ny));
+        }
+        if board[ny][nx] == ']' {
+            to_move.push((nx - 1, ny));
+        }
+
+        while let Some((x, y)) = to_move.pop() {
+            let surface = if dir.0 == 0 {
+                vec![(x, y), (x + 1, y)]
+            } else {
+                if dir.0 == -1 { vec![(x, y)] }
+                else { vec![(x + 1, y)] }
+            };
+
+            for (sx, sy) in surface {
+                let (dx, dy) = ((sx as i32 + dir.0) as usize, (sy as i32 + dir.1) as usize);
+                match board[dy][dx] {
+                    '#' => return false,
+                    '[' => {
+                        if !to_move.contains(&(dx, dy)) {
+                            to_move.push((dx, dy));
+                        }
+                    },
+                    ']' => {
+                        if !to_move.contains(&(dx - 1, dy)) {
+                            to_move.push((dx - 1, dy));
+                        }
+                    },
+                    _ => {},
+                };
+            }
+
+            do_move.push((x, y));
+        }
+
+        for (x, y) in do_move.iter().rev() {
+            let (dx, dy) = ((*x as i32 + dir.0) as usize, (*y as i32 + dir.1) as usize);
+            board[dy][dx] = '[';
+            board[dy][dx + 1] = ']';
+            match dir {
+                (-1, 0) => { board[*y as usize][*x as usize + 1] = '.'; },
+                (1, 0) => { board[*y as usize][*x as usize] = '.'; },
+                (0, _) => {
+                    board[*y as usize][*x as usize] = '.';
+                    board[*y as usize][*x as usize + 1] = '.';
+                },
+                _ => panic!("invalid direction"),
+            }
+        }
+
+        return true;
+    }
 
     false
 }
@@ -117,16 +174,8 @@ fn inflate(board: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     inflated
 }
 
-fn part_1(board: &Vec<Vec<char>>, moves: &Vec<char>) -> i64 {
+fn apply_moves(board: &Vec<Vec<char>>, moves: &Vec<char>) -> i64 {
     let mut board = board.clone();
-    for m in moves {
-        move_robot(&mut board, *m);
-    }
-    score(&board)
-}
-
-fn part_2(board: &Vec<Vec<char>>, moves: &Vec<char>) -> i64 {
-    let mut board = inflate(&board);
     for m in moves {
         move_robot(&mut board, *m);
     }
@@ -135,6 +184,6 @@ fn part_2(board: &Vec<Vec<char>>, moves: &Vec<char>) -> i64 {
 
 fn main() {
     let (board, moves) = parse(&aoc::input_lines());
-    println!("Part I: {}", part_1(&board, &moves));
-    println!("Part II: {}", part_2(&board, &moves));
+    println!("Part I: {}", apply_moves(&board, &moves));
+    println!("Part II: {}", apply_moves(&inflate(&board), &moves));
 }
